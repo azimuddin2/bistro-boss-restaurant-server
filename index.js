0db8ad52bd9d6295fc826291692f6941041a849a
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-10-21 23:32:17 +0600

    added user delete and patch

--- index.js
+++ index.js
@@ -46,6 +46,27 @@ async function run() {
       res.send(users);
     });
 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
     // menu operation
     app.get('/menu', async (req, res) => {
       const query = {};
