b5944d39ce916d5bf29ca9a8fc709fc8ee0f56cb
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-11-13 23:31:48 +0600

    admin-stats api operation

--- index.js
+++ index.js
@@ -292,6 +292,23 @@ async function run() {
     });
 
 
+    app.get('/admin-stats', verifyJWT, verifyAdmin, async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment) => sum + payment.price, 0);
+
+      res.send({
+        revenue,
+        users,
+        menuItems,
+        orders
+      });
+    });
+
+
   } finally {
 
   }
